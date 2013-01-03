689f1289cdb055e181c17e1b5ceab1692f2d4f4d
Alex Thomas <isszul@gmail.com>
2013-01-03 21:33:31 +0000

    Removed some other files too

--- WebContent/client/js/views/desktop/DesktopView.js
+++
@@ -1,8 +0,0 @@
-
-define(["jquery", "underscore", "backbone.marionette", "text!templates/desktop/desktopTemplate.html", "translationUtil"], function($, _, Marionette, desktopTemplate, TranslationUtil) {
-  var DesktopView;
-  DesktopView = Marionette.ItemView.extend({
-    template: TranslationUtil.geti18nTemplate(desktopTemplate)
-  });
-  return DesktopView;
-});
