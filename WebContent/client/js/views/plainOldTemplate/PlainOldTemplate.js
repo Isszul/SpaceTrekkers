ca2e7cf40abc6d5744fc3b416c3c147de74c033f
isszul <Isszul@gmail.com>
2013-01-13 18:33:06 +0000

    Moved LoginView and SignupView into user modules.
    Signed-off-by: isszul <Isszul@gmail.com>

--- WebContent/client/js/views/plainOldTemplate/PlainOldTemplate.js
+++ WebContent/client/js/views/plainOldTemplate/PlainOldTemplate.js
@@ -3,13 +3,13 @@ define(["jquery", "underscore", "backbone.marionette", "translationUtil"], funct
   var plainOldTemplateView;
   plainOldTemplateView = Marionette.ItemView.extend({
     template: "<div>" + TranslationUtil.geti18nString('loading' + "</div>"),
-    initialize: function(templateFileName) {
+    initialize: function(templateFileName, options) {
       var _template;
       _template = $.ajax({
         url: templateFileName,
         async: false
       });
-      this.template = TranslationUtil.geti18nTemplate(_template.responseText);
+      this.template = TranslationUtil.geti18nTemplate(_template.responseText, options);
       return this;
     }
   });
